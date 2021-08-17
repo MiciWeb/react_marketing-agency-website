5bd929f4cbf3dab50f5485bf08fd650c50a29e78
Mici Srsr <micipsa.sersour@epitech.eu>
2021-08-17 17:51:26 +0200

    add a bug trying to fix it

---
+++ src/components/Navbar/Navbar.elements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Nav = styled.nav`
+display: flex;
+justify-content: center;
+align-items: center;
+background: #101522;
+height: 80px;
+font-size: 1.2rem;
+position: sticky;
+top: 0;
+z-index:999;
+`
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Nav } from "./Navbar.elements"
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                Test
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
+
