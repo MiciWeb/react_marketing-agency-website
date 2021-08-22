c0a3064c13a5313cdb40dae7040d0509ea06d0e2
Mici Srsr <micipsa.sersour@epitech.eu>
2021-08-22 10:52:51 +0200

    add responsive menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import { Nav, NavbarContainer, NavLogo, NavIcon, MobileIcon } from "./Navbar.elements"
+import { FaBars, FaTimes } from "react-icons/fa"
+import { IconContext } from "react-icons/lib"
 
 
 const Navbar = () => {
+
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => {
+        setClick(!click)
+    }
+
     return (
         <>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to="/">
-                        <NavIcon />
+            <IconContext.Provider value={{ color: "#fff" }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to="/">
+                            <NavIcon />
                         ULTRA
                         </NavLogo>
-                    <MobileIcon>
-                        {click ? <FaTimes /> : <FaBars />}
-                    </MobileIcon>
-                </NavbarContainer>
-            </Nav>
+                        <MobileIcon onClick={handleClick} >
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
         </>
     )
 }
