752c3a0e7fb4b28aab33db2e1fa7065623c57c3a
Mici Srsr <micipsa.sersour@epitech.eu>
2021-08-26 05:06:00 +0200

    fix redirecting problem

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Services from "./pages/Services/Services"
 import Products from "./pages/Products/Products"
 import SignUp from "./pages/SignUp/SignUp"
 import ScrollToTop from "./components/ScrollToTop"
+import { Redirect } from 'react-router';
+
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <GlobalStyle />
       <ScrollToTop />
       <Navbar />
+      <Route exact path="/react_marketing-agency-website">
+         <Redirect to="/" />
+      </Route>
       <Switch>
         <Route path="/" exact={true} component={Home} />
         <Route path="/services" exact={true} component={Services} />
